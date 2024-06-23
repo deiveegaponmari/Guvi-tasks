e56b915f2a3b284cfb9e00fd6a6ed7d973b13208
ponmarimdeiveega@gmail.com <ponmarimdeiveega@example.com>
2024-06-23 15:37:09 +0530

    changes success.

--- Day3/Task1/odd.js
+++ Day3/Task1/odd.js
@@ -1,4 +1,4 @@
-let data=[1,2,3,4,5];
+ let data=[1,2,3,4,5];
 const output=(function(input){
     for(i=0;i<input.length;i++){
         if(input[i]%2!==0){
@@ -6,5 +6,7 @@ const output=(function(input){
         }
     }
 
-})(data)
-console.log(output);
+})(data) 
+
+
+//console.log(output);
--- Day3/Task2/odd.js
+++ Day3/Task2/odd.js
@@ -1,14 +1,14 @@
- const data=[1,2,3,4,5];
+/*  const data=[1,2,3,4,5];
  const oddnumbers=data.filter(num => num%2!==0);
- console.log(oddnumbers);
+ console.log(oddnumbers); */
 
-
-    /* console.log(data);
-    for(i=0;i<data.length;i++){
-        if(data[i]%2!==0){
-          console.log(data[i]);
-        }
-    }
-
-}
-console.log(output); */
+ let data=[1,2,3,4,5];
+ const output=((input) => {
+     for(i=0;i<input.length;i++){
+         if(input[i]%2!==0){
+           console.log(input[i]);
+         }
+     }
+ 
+ })(data)
+//console.log(output);
